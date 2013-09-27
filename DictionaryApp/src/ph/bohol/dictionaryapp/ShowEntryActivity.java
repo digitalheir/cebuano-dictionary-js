9c8d00202336f6632d39dd7e32daf79fb152af5c
Jeroen Hellingman <jeroen@bohol.ph>
2013-09-27 10:42:59 +0200

    Renamed ShowEntryActivity related layout xml.

--- DictionaryApp/src/ph/bohol/dictionaryapp/ShowEntryActivity.java
+++ DictionaryApp/src/ph/bohol/dictionaryapp/ShowEntryActivity.java
@@ -39,7 +39,7 @@ public class ShowEntryActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search_results);
+		setContentView(R.layout.activity_show_entry);
 		
 		// Show the Up button in the action bar.
 		setupActionBar();
