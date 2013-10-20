39bae0fd67ed93c6742dfa9c4bf1a22347cdbe37
Jeroen Hellingman <jeroen@bohol.ph>
2013-10-20 23:35:03 +0200

    Adding various resource files (images); removing accidentally checked in class files.

---
+++ DictionaryApp/src/ph/bohol/dictionaryapp/AboutDialog.java
@@ -0,0 +1,71 @@
+package ph.bohol.dictionaryapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.util.Linkify;
+
+import android.widget.TextView;
+
+/**
+ * AboutDialog shows some information about the dictionary app.
+ * @author Jeroen Hellingman.
+ *
+ */
+public class AboutDialog extends Dialog
+{
+    private static Context context = null;
+
+    /**
+     * Create a new AboutDialog.
+     * @param newContext the context to be used.
+     */
+    public AboutDialog(final Context newContext)
+    {
+        super(newContext);
+        AboutDialog.context = newContext;
+    }
+
+    @Override
+    public final void onCreate(final Bundle savedInstanceState)
+    {
+        setContentView(R.layout.about_dialog);
+
+        // TextView textView = (TextView) findViewById(R.id.legal_text);
+        // textView.setText(readRawTextFile(R.raw.legal));
+
+        TextView textView = (TextView) findViewById(R.id.info_text);
+        textView.setText(Html.fromHtml(readRawTextFile(R.raw.about)));
+        textView.setLinkTextColor(Color.WHITE);
+        Linkify.addLinks(textView, Linkify.ALL);
+    }
+
+    private static String readRawTextFile(final int id)
+    {
+        InputStream inputStream = context.getResources().openRawResource(id);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        StringBuilder text = new StringBuilder();
+        try
+        {
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                text.append(line);
+            }
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+        return text.toString();
+    }
+}
